0f919f4ec822c509fd023cb2c5976d7ea3248f8a
victormogollon <victor.lima.ti@gmail.com>
2024-10-16 23:05:02 -0300

    commit 3

--- DesafioControleFluxo/src/contador.java
+++ DesafioControleFluxo/src/contador.java
@@ -20,12 +20,14 @@ public class contador {
     }
 
     static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
+
+        int dif = 0;
+        
         if (parametro1 > parametro2) {
             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }else{
+                 dif = parametro2 - parametro1;
         }
-
-        int dif = parametro2 - parametro1;
-
         for (int i = 1; i <= dif; i++) {
             System.out.println(i);
         }
