c97862b65e2a8714ad94e8d29bf08c322bdb1ec9
victormogollon <victor.lima.ti@gmail.com>
2024-10-16 23:00:37 -0300

    2 commit

--- DesafioControleFluxo/src/contador.java
+++ DesafioControleFluxo/src/contador.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int param1, param2;
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        param1 = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        param2 = terminal.nextInt();
+
+        try {
+            contar(param1, param2);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
+        if (parametro1 > parametro2) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int dif = parametro2 - parametro1;
+
+        for (int i = 1; i <= dif; i++) {
+            System.out.println(i);
+        }
     }
 }
+
+
